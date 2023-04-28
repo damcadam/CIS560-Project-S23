241c4096915d002a7077b9ca7adc494c4b9bf311
Isaiah McAdam <isaiahm@ksu.edu>
2023-04-28 12:10:37 -0500

    Able to insert tables from TSV, but need to still get exact inserts and there order figured out. Also moved TSV data and format files to the SQL -> Data folder.

--- CIS-560-Project-Team-16/CIS-560-Project-Team-16/Sql/Tables/Directors.sql
+++ CIS-560-Project-Team-16/CIS-560-Project-Team-16/Sql/Tables/Directors.sql
@@ -1,5 +1,12 @@
-﻿CREATE TABLE MovieDatabase.Directors
+﻿--DROP TABLE MovieDatabase.Directors;
+
+CREATE TABLE MovieDatabase.Directors
 (
-	directorID INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	directorId int IDENTITY(1,1) PRIMARY KEY,
 	fullName NVARCHAR(128)  NOT NULL
 );
+
+--SELECT *
+--FROM MovieDatabase.Directors
+
+-- bcp CIS560Project.MovieDatabase.Directors in "C:\Users\Isaiah\source\repos\CIS560-Project-S23\CIS-560-Project-Team-16\CIS-560-Project-Team-16\Sql\Data\MovieOutput.tsv" -S "(localdb)\MSSQLLocalDb" -T -f DirectorFormat.fmt -h "CHECK_CONSTRAINTS"
--- CIS-560-Project-Team-16/CIS-560-Project-Team-16/Sql/Tables/Genre.sql
+++ CIS-560-Project-Team-16/CIS-560-Project-Team-16/Sql/Tables/Genre.sql
@@ -1,5 +1,13 @@
-﻿CREATE TABLE MovieDatabase.Genre
+﻿--(localdb)\MSSQLLocalDb
+--DROP TABLE MovieDatabase.Genre;
+
+CREATE TABLE MovieDatabase.Genre
 (
 	genreId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	movieId INT NOT NULL FOREIGN KEY REFERENCES MovieDatabase.Movies(movieId)
 );
+
+--SELECT *
+--FROM MovieDatabase.Genre
+
+-- bcp CIS560Project.MovieDatabase.Directors in "C:\Users\Isaiah\source\repos\CIS560-Project-S23\CIS-560-Project-Team-16\CIS-560-Project-Team-16\Sql\Data\StaticGenres.tsv" -S "(localdb)\MSSQLLocalDb" -T -f StaticGenres.fmt -h "CHECK_CONSTRAINTS"
