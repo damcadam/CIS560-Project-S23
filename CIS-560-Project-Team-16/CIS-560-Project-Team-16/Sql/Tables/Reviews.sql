365ff7c3a28d818faf339e1eeeb13e9b86d329b2
Isaiah McAdam <isaiahm@ksu.edu>
2023-04-28 22:26:20 -0500

    Got WatchList table fixed.

--- CIS-560-Project-Team-16/CIS-560-Project-Team-16/Sql/Tables/Reviews.sql
+++ CIS-560-Project-Team-16/CIS-560-Project-Team-16/Sql/Tables/Reviews.sql
@@ -1,7 +1,22 @@
-﻿CREATE TABLE MovieDatabase.Reviews
-(
-	reviewId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-	-- Average score our of count reivews from 0 to 10
-	averageIMDb INT NOT NULL,
-	[count] INT NOT NULL,
-);
+﻿--(localdb)\MSSQLLocalDb
+DROP TABLE MovieDatabase.Reviews;
+
+IF OBJECT_ID(N'MovieDatabase.Reviews') IS NULL
+BEGIN
+	CREATE TABLE MovieDatabase.Reviews
+	(
+		reviewId int IDENTITY(1,1) PRIMARY KEY,
+		movieId INT --NOT NULL??????????????????????????????????????????????????????????????
+			FOREIGN KEY REFERENCES MovieDatabase.Movies(movieId),
+		averageIMDB NVARCHAR(128) NOT NULL,
+		ratingVotes NVARCHAR(128) NOT NULL,
+	);
+END;
+
+--create the relationship table for Languages
+
+SELECT *
+FROM MovieDatabase.Reviews
+
+
+-- bcp CIS560Project.MovieDatabase.Reviews in "C:\Users\Isaiah\source\repos\CIS560-Project-S23\CIS-560-Project-Team-16\CIS-560-Project-Team-16\Sql\Data\MovieOutput.tsv" -S "(localdb)\MSSQLLocalDb" -T -f ReviewsFormat.fmt -h "CHECK_CONSTRAINTS" -F 2
